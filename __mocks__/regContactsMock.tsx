a66768629445ddb18e5fb5da13d7dd0f96e6d28d
Antonius Anggito Arissaputro <antonius.anggito@ui.ac.id>
2023-09-14 18:38:55 +0700

    Update regContactsMock data for testing

--- __mocks__/regContactsMock.tsx
+++ __mocks__/regContactsMock.tsx
@@ -29,11 +29,18 @@ export const regContactsMock = {
       __typename: 'contact',
     },
   ],
+  contact_aggregate: {
+    aggregate: {
+      count: 2,
+      __typename: 'contact_aggregate_fields',
+    },
+    __typename: 'contact_aggregate',
+  },
 };
 
 export const MockRegContactsComponent: React.FC = () => {
   const { data, loading, error } = useGetRegContactsQuery({
-    variables: { offset: 0, limit: 5, favIds: [4] },
+    variables: { offset: 0, limit: 5, favIds: [4], like: '%%' },
   });
   if (loading) return <p>Loading...</p>;
   if (error) return <p>{error.message}</p>;
@@ -50,7 +57,7 @@ export const successRegContactsMock = [
   {
     request: {
       query: GET_REG_CONTACTS,
-      variables: { offset: 0, limit: 5, favIds: [4] },
+      variables: { offset: 0, limit: 5, favIds: [4], like: '%%' },
     },
     result: {
       data: regContactsMock,
@@ -62,7 +69,7 @@ export const errorRegContactsMock = [
   {
     request: {
       query: GET_REG_CONTACTS,
-      variables: { offset: 0, limit: 5, favIds: [4] },
+      variables: { offset: 0, limit: 5, favIds: [4], like: '%%' },
     },
     error: new Error('An error occured'),
   },
