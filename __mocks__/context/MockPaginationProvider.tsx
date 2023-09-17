eecdb21accf35498ae478b2a0dfeeb257239dec4
Antonius Anggito Arissaputro <antonius.anggito@ui.ac.id>
2023-09-17 22:08:44 +0700

    Remove unnecessary imports

--- __mocks__/context/MockPaginationProvider.tsx
+++ __mocks__/context/MockPaginationProvider.tsx
@@ -1,7 +1,6 @@
 import React, { useState } from 'react';
 import {
   PaginationContext,
-  PaginationProviderProps,
   PaginationType,
 } from '../../components/context/paginationContext';
 
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,4 @@
 import type { NextPage } from 'next';
-import Head from 'next/head';
-
-import styled from '@emotion/styled';
 import SearchInput from '../components/Contacts/SearchInput';
 import ContactsSection from '../components/Contacts';
 import FavProvider from '../components/context/favContext';
